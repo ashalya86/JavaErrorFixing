5eaed8ea4a46fdcfffb1d1f9d8cda442b793c5e0
ashalya86 <ashalya86@users.noreply.github.com>
2024-05-03 22:09:42 +0530

    Add files via upload

---
+++ ReadFiles/StringBufferExample2.java
@@ -0,0 +1,8 @@
+public class StringBufferExample2 {
+    public static void main(String[] args) {
+        String stringBuffer = "";
+        for (int i = 0; i < 5; i++) {
+            stringBuffer += "Number: " + i;
+        }
+    }
+}
